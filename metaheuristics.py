a3a1124a090ca01f5b2986da615bb14c8dc40cc5
Damian Jordanov <d.jordanov@posteo.net>
2020-12-06 21:05:56 +0100

    new files

--- metaheuristics.py
+++ metaheuristics.py
@@ -7,28 +7,6 @@ import pandas as pd
 
 import tsplib95
 
-def generate_random_euclidean__instances(count: int, cnodes: int, squaresize: int):
-    # use tsplib format for easy use of tsplib95 imports
-
-    print("Generating instances...")
-    for i in range(count):
-
-        # create file and compulsories
-        name = "rnd" + str(i) + "_" + str(cnodes) + ".tsp"
-        f = open("./instances/" + name, "w+")
-        f.write("NAME : " + name + "\n")
-        f.write("TYPE : TSP" + "\n")
-        f.write("DIMENSION : " + str(cnodes) + "\n")
-        f.write("EDGE_WEIGHT_TYPE : EUC_2D" + "\n")
-        f.write("NODE_COORD_SECTION" + "\n")
-
-        # generate coordinates
-        for inode in range(cnodes):
-            line = str(inode) + " " + str(random.randint(0, squaresize)) + " " + str(random.randint(0, squaresize))
-            f.write(line + "\n")
-
-        f.close()
-
 # Quality evaluation wrapper for counting quality evaluations
 class Eval:
     evals_count: int = 0
