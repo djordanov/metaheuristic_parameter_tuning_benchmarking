933bc820871dd466c68d827efcc8f4e42e0b4cdd
Damian Jordanov <d.jordanov@posteo.net>
2021-01-06 10:37:23 +0100

    added analyze functions for convergence plots and evals2time plot

---
+++ myproject/analyze.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+# plot convergence
+def convergence(df: pd.DataFrame):
+    # clean dataframe
+    df = df.drop('time', axis = 'columns')
+
+    # compute mean
+    dfg = df.groupby('evals')
+    dfgm = dfg.mean()
+
+    # create plot
+    plt.plot(dfgm.index, dfgm.qualdev)
+    plt.xlabel('Number of Quality Evaluations')
+    plt.ylabel('Quality Deviation from Optimum')
+    plt.semilogx()
+    plt.show()
+
+def evals2time(df: pd.DataFrame):
+    # decimate the data
+    dfs = df.sample(100000)
+
+    # linear regression ...
+    y = dfs.time
+    x = dfs[['evals']]
+    x = sm.add_constant(x)
+
+    model = sm.OLS(y, x).fit()
+    intercept = model.params.const
+    evals = model.params.evals
+
+    plt.scatter(dfs.evals, dfs.time, marker = 'o', color = 'cornflowerblue', alpha = 0.5, label = 'empirical result sa')
+    plt.plot(dfs.evals, intercept + evals * dfs.evals, color = 'black', linewidth = '2', alpha = 1, label = 'linear regression sa')
+    plt.ylabel('Wall Clock Time in Seconds')
+    plt.xlabel('Number of Quality Evaluations')
+    plt.legend()
+    plt.show()
+
