dc7a8b04d86482eb741550e4ebc76ee1af69ef78
Damian Jordanov <d.jordanov@posteo.net>
2021-01-07 12:33:25 +0100

    use concorde and r concorde wrapper to optimally solve TSP, regenerate euclidean tsp instances

--- myproject/generate_instances.py
+++ myproject/generate_instances.py
@@ -1,14 +1,23 @@
 import random
+from pathlib import Path
+import tsplib95
+
+import numpy as np
+import pandas as pd
+from python_tsp.exact import solve_tsp_dynamic_programming
+
+import os
+from rpy2 import robjects
+
+CONCORDE_PATH = Path('myproject/')
 
 def generate_optimal_solutions(instancefolder: str):
 
-    from pathlib import Path
-    import tsplib95
-    import numpy as np
-    import pandas as pd
-    from python_tsp.exact import solve_tsp_dynamic_programming
-
     print('--- Computing optimal solutions... ---')
+
+    # solve with concorde
+    robjects.r('library("TSP")')
+    robjects.r('concorde_path(\"' + str(CONCORDE_PATH.absolute()) + '\")')
 
     # iterate over instances
     entries = Path(instancefolder)
@@ -17,39 +26,35 @@ def generate_optimal_solutions(instancefolder: str):
         if entry.suffix != '.tsp':
             continue
 
-        # load problem and get distances
-        print(entry.absolute())
-        problem = tsplib95.load(entry.absolute())
-        cnodes = len(list(problem.get_nodes()))
-        distances = [ [problem.get_weight(a, b) for b in range(cnodes)] for a in range(cnodes) ]
-
-        # compute optimal solution
-        distance_matrix = np.array(distances)
-        permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
-
+        # solve with concorde
+        robjects.r('problem = read_TSPLIB(\"' + str(entry.absolute()) +  '\")')
+        robjects.r('tour = solve_TSP(problem, method = "concorde")')
+        tour = list(robjects.r('as.integer(tour)'))
+        
         # save optimal solution
-        print(str(permutation))
-        print(distance)
-        optpath = Path(instancefolder + entry.name).with_suffix('.opt.tour')
+        optpath = Path(instancefolder + '/' + entry.name).with_suffix('.opt.tour')
         fopt = optpath.open('w+')
         fopt.write('NAME : ' + entry.name + '.tsp.optbc.tour\n')
         fopt.write('TYPE : TOUR\n')
-        fopt.write('DIMENSION : '+ str(problem.dimension) + '\n')
+        fopt.write('DIMENSION : '+ str(len(tour)) + '\n')
         fopt.write('TOUR_SECTION''\n')
-        for node in permutation:
+        for node in tour:
             fopt.write(str(node) + '\n')
         fopt.write('-1')
         fopt.close()
 
-def generate_random_euclidean__instances(count: int, cnodes: int, squaresize: int):
-    # use tsplib format for easy use of tsplib95 imports
+def generate_random_euclidean_instances(dir: Path, count: int, cnodes: int, squaresize: int):
+    # use tsplib format
 
-    print("Generating instances...")
+    # create directory if not exists
+    dir.mkdir(parents = True, exist_ok = True)
+
+    # create problems...
     for i in range(count):
 
         # create file and compulsories
         name = "rnd" + str(i) + "_" + str(cnodes) + ".tsp"
-        f = open("./instances/" + name, "w+")
+        f = open(str(dir.absolute()) + '/' + name, "w")
         f.write("NAME : " + name + "\n")
         f.write("TYPE : TSP" + "\n")
         f.write("DIMENSION : " + str(cnodes) + "\n")
@@ -57,7 +62,25 @@ def generate_random_euclidean__instances(count: int, cnodes: int, squaresize: in
         f.write("NODE_COORD_SECTION" + "\n")
 
         # generate coordinates
-        for inode in range(cnodes):
+        for inode in range(1, cnodes + 1):
             line = str(inode) + " " + str(random.randint(0, squaresize)) + " " + str(random.randint(0, squaresize))
             f.write(line + "\n")
         f.close()
+
+generate_random_euclidean_instances(Path('myproject/instances/50nodes/'), count = 50, cnodes = 50, squaresize = 1000)
+generate_optimal_solutions('myproject/instances/50nodes')
+generate_random_euclidean_instances(Path('myproject/instances/50nodes/test'), count = 50, cnodes = 50, squaresize = 1000)
+generate_optimal_solutions('myproject/instances/50nodes/test')
+os.system('ls myproject/instances/50nodes/ *.tsp | sort > myproject/instances/50nodes/trainInstancesFile')
+
+generate_random_euclidean_instances(Path('myproject/instances/20nodes'), count = 50, cnodes = 20, squaresize = 1000)
+generate_optimal_solutions('myproject/instances/20nodes')
+generate_random_euclidean_instances(Path('myproject/instances/20nodes/test'), count = 50, cnodes = 20, squaresize = 1000)
+generate_optimal_solutions('myproject/instances/20nodes/test')
+os.system('ls myproject/instances/20nodes/ *.tsp | sort > myproject/instances/50nodes/trainInstancesFile')
+
+generate_random_euclidean_instances(Path('myproject/instances/100nodes'), count = 50, cnodes = 100, squaresize = 1000)
+generate_optimal_solutions('myproject/instances/100nodes')
+generate_random_euclidean_instances(Path('myproject/instances/100nodes/test'), count = 50, cnodes = 100, squaresize = 1000)
+generate_optimal_solutions('myproject/instances/100nodes/test')
+os.system('ls myproject/instances/100nodes/ *.tsp | sort > myproject/instances/50nodes/trainInstancesFile')
