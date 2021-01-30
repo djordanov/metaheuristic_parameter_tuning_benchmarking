77eab3f2aa60b674a14781cbefeb98b2086ad2b9
Damian Jordanov <d.jordanov@posteo.net>
2021-01-30 23:30:56 +0100

    analyzing and preprocessing

--- myproject/analyze.py
+++ myproject/analyze.py
@@ -38,3 +38,31 @@ def evals2time(df: pd.DataFrame):
     plt.legend()
     plt.show()
 
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+irace_traj_fnames = {
+    'SA': {
+        1000: "myproject/data/irace/test5000-SA-{'qualdev': 0, 'evals': 1000}-qualdev-run-trajectory.csv",
+        10000: "myproject/data/irace/test5000-SA-{'qualdev': 0, 'evals': 10000}-qualdev-run-trajectory.csv"
+    },
+    'GA': {
+        1000: "myproject/data/irace/test5000-GA-{'qualdev': 0, 'evals': 1000}-qualdev-run-trajectory.csv",
+        10000: "myproject/data/irace/test5000-GA-{'qualdev': 0, 'evals': 10000}-qualdev-run-trajectory.csv"
+    },
+    'ACO': {
+        1000: "myproject/data/irace/test5000-ACO-{'qualdev': 0, 'evals': 1000}-qualdev-run-trajectory.csv",
+        10000: "myproject/data/irace/test5000-ACO-{'qualdev': 0, 'evals': 10000}-qualdev-run-trajectory.csv"
+    }
+}
+
+for algorithm in irace_traj_fnames.keys():
+    for evals in irace_traj_fnames[algorithm].keys():
+        df = pd.read_csv(irace_traj_fnames[algorithm][evals])
+        label = '{} with {} evals'.format(algorithm, str(evals))
+        plt.plot(df.index, df.incumbent_mean_sofar, label = label)
+    
+plt.xlabel('Number of Metaheuristic Runs')
+plt.ylabel('Estimated Performance of best Configuration')
+plt.legend()
+plt.show()
