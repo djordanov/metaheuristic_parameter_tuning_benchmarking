2997b1e2033ddf8df11ae42806515dc6cce5f7b9
Damian Jordanov <d.jordanov@posteo.net>
2021-03-14 22:03:25 +0100

    minor visual update to plot

--- myproject/analyze/linear_regression.py
+++ myproject/analyze/linear_regression.py
@@ -1,6 +1,8 @@
 from myproject.helpers import BASE_TERM, DEF_CFGS, cmhrun_fname
 import pandas as pd
+
 import matplotlib.pyplot as plt
+from matplotlib import ticker
 
 from sklearn.linear_model import LinearRegression
 
@@ -11,10 +13,11 @@ time2evals = {
     'coefficient': []
 }
 
+fig, ax = plt.subplots()
 for algorithm, config in DEF_CFGS.items():
     
     # linear regression time -> evals
-    fname = cmhrun_fname(algorithm, config, BASE_TERM, 'qualdev')
+    fname = cmhrun_fname(algorithm, config, BASE_TERM)
     df = pd.read_csv('myproject/data/conv/' + fname + '.csv')
     x = df.time.values.reshape(-1, 1)
     y = df.evals.values.reshape(-1, 1)
@@ -32,14 +35,15 @@ for algorithm, config in DEF_CFGS.items():
     # scatter plot
     grouped_by_evals = df.groupby('evals')
     evals_mean_time = grouped_by_evals.mean()
-    plt.scatter(evals_mean_time.time, evals_mean_time.index, label = algorithm)
+    ax.scatter(evals_mean_time.time, evals_mean_time.index, label = algorithm)
     
 # save model values
 pd.DataFrame(time2evals).to_csv('myproject/data/time2evals.csv', index = None)
     
-plt.xlabel('Elapsed Wall Clock Time in Seconds')
-plt.ylabel('Number of Tour Quality Evaluations')
-plt.legend()
-plt.tight_layout()
-plt.savefig('myproject/data/figures/time2evals.png')
+ax.set_xlabel('Elapsed Wall Clock Time in Seconds')
+ax.set_ylabel('Number of Tour Quality Evaluations')
+ax.get_yaxis().set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
+ax.legend()
 
+fig.tight_layout()
+fig.savefig('myproject/data/figures/time2evals.png')
